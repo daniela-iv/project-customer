25b3d44d60551334ada8e271ee4dea6f4e1a264f
daniela-iv <voicuioanadaniela@yahoo.com>
2024-09-10 09:21:44 +0200

    Added player camera + movement

---
+++ project-customer/Assets/MouseLook.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum Direction
+    {
+        None,
+        Horizontal = (1 << 0),
+        Vertical = (1 << 1)
+    }
+    [SerializeField] private Direction directions;
+    [SerializeField] private Vector2 acceleration;
+    [SerializeField] private Vector2 sensitivity;
+     private Vector2 velocity;
+     private Vector2 rotation;
+
+    [SerializeField] private float maxVerticalAngle;
+
+    private Vector2 lastInput;
+    [SerializeField] private float inputLagPeriod;
+    private float inputLagTimer;
+
+    private void OnEnable()
+    {
+        velocity = Vector3.zero;
+        inputLagTimer = 0;
+        lastInput = Vector3.zero;
+
+        Vector3 euler = transform.localEulerAngles;
+
+        if (euler.x >= 180)
+        {
+            euler.x -= 360;
+        }
+        euler.x = Mathf.Clamp(euler.x, -maxVerticalAngle, maxVerticalAngle);
+
+        rotation= new Vector2(euler.x, euler.y);
+    }
+
+    private Vector2 GetInput()
+    {
+        inputLagTimer += Time.deltaTime;
+
+        Vector2 input = new Vector2(
+            Input.GetAxis("Mouse X"),
+            Input.GetAxis("Mouse Y")
+            );
+
+        if ((Mathf.Approximately(0, input.x) && Mathf.Approximately(0, input.y)) == false || 
+            inputLagTimer >= inputLagPeriod) 
+        {
+            lastInput = input;
+            inputLagTimer = 0;
+        }
+
+        return lastInput;
+    }
+
+    private void Update()
+    {
+        Vector2 appliedVelocity = GetInput() * sensitivity;
+
+        if((directions & Direction.Horizontal) == 0)
+        {
+            appliedVelocity.x = 0;
+        }
+        if((directions & Direction.Vertical) == 0)
+        {
+            appliedVelocity.y = 0;
+        }
+
+        velocity = new Vector2(
+            Mathf.MoveTowards(velocity.x, appliedVelocity.x, acceleration.x * Time.deltaTime),
+            Mathf.MoveTowards(velocity.y, appliedVelocity.y, acceleration.y * Time.deltaTime));
+        
+        rotation += velocity * Time.deltaTime;
+        rotation.y= Mathf.Clamp(rotation.y,-maxVerticalAngle, maxVerticalAngle);
+        
+        transform.localEulerAngles = new Vector3(rotation.y, rotation.x, 0);
+        }
+}
+
---
+++ project-customer/Assets/PlayerControls.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ControlType { Force, Position, Velocity }
+
+public class SimplePhysicsControls : MonoBehaviour
+{
+
+    public ControlType control;
+
+    public float moveForce;
+    public float moveSpeed;
+    public float jumpForce = 20;
+
+    public float rayLength = 1;
+
+    Rigidbody rb;
+    bool grounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.sleepThreshold = 0;
+    }
+
+    private void Update()
+    {
+        if (IsGrounded() &&
+            Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity += new Vector3(0, jumpForce, 0);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit hitInfo;
+        //Debug.DrawRay(transform.position, Vector3.down * 1.1f,Color.yellow);
+        //transform.parent = null;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, 1.1f))
+        {
+            //GetComponent<ChangeColor>().SetColor(Color.blue);
+            //if (hitInfo.collider.GetComponent<MovePlatform>() != null) { /*transform.parent = hitInfo.transform;*/ }
+            return true;
+        }
+        return false;
+    }
+    private Vector3 GetCameraRelativeVector()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight= Camera.main.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        
+        camForward = camForward.normalized;
+        camRight= camRight.normalized;
+
+        Vector3 relativeForward = playerInput.x * camForward;
+        Vector3 relativeRight = playerInput.y * camRight;
+
+        Vector3 relativePosition = relativeForward + relativeRight;
+
+        return relativePosition;
+    }
+
+    void FixedUpdate()
+    {
+        //add func here
+        Vector3 moveVector = GetCameraRelativeVector();
+            //new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+
+        //transform.Translate(moveVector * moveSpeed);
+
+        if (IsGrounded())
+        {
+            control = ControlType.Velocity;
+        }
+        else
+        {
+            control = ControlType.Force;
+        }
+
+        switch (control)
+        {
+            case ControlType.Force:
+                rb.AddForce(moveVector * moveForce);
+                break;
+            case ControlType.Velocity:
+                Vector3 newVelocity = new Vector3(0, rb.velocity.y, 0) + moveVector * moveSpeed;
+                rb.velocity = newVelocity;
+                break;
+        }
+        grounded = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        //Debug.DrawRay(collision.GetContacts(0).point, collision.GetContacts(0).normal * rayLength, Color.magenta);
+
+        if (collision.GetContact(0).normal.y > 0.8f)
+        {
+            //GetComponent<ChangeColor>().SetColor(Color.red);
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //grounded = false;
+    }
+}
+
