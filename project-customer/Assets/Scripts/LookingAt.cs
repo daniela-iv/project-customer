bba5af3ff7166a7664d7acacdb045e9afb5685a8
GerranRoord <gerranroord@gmail.com>
2024-09-23 15:40:46 +0200

    f popup changes

--- project-customer/Assets/Scripts/LookingAt.cs
+++ project-customer/Assets/Scripts/LookingAt.cs
@@ -22,6 +22,8 @@ public class LookingAt : MonoBehaviour
     private bool isInspecting;
 
     private string inspectTextItem = "(F) Inspect ";
+    private string inspectTextItemExit = "(F) Leave ";
+    private string inspectTextItemSwitch = "(F) Turn off power ";
     private string inspectTextDoor = "(F) To open ";
     private string inspectTextNPC = "(F) Talk with ";
 
@@ -59,7 +61,20 @@ public class LookingAt : MonoBehaviour
                 if (!isInspecting)
                 {
                     inspectableUI.SetActive(true);
-                    inspectableUI.GetComponentInChildren<TextMeshProUGUI>().text = inspectTextItem + hit.transform.GetComponent<Item>().Name;
+                    Item item = hit.transform.GetComponent<Item>();
+                    TextMeshProUGUI text = inspectableUI.GetComponentInChildren<TextMeshProUGUI>();
+                    if (item.interactionType == Item.InteractionType.Switch)
+                    {
+                        text.text = inspectTextItemSwitch;
+                    }
+                    else if(item.interactionType == Item.InteractionType.exit)
+                    {
+                        text.text = inspectTextItemExit;
+                    }
+                    else
+                    {
+                        text.text = inspectTextItem + item.Name;
+                    }
                 }
 
                 if (Input.GetButtonDown("Interact"))
